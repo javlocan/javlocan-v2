c48e00d1e8f09b97d61a187bb7628a38943bcbaf
javlocan <javlpzcntr@gmail.com>
2023-07-02 12:09:17 +0200

    Basic Navbar/Landing

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import styles from "./NavBar.module.css";
+
 export const NavBar = () => {
   return (
     <>
-      <h1>NavBar</h1>
+      <div className={styles.container} id="container">
+        <div className={styles.name}>
+          <h1 className={styles.javier}>
+            jav<text>ier </text>
+          </h1>
+          <h1 className={styles.lopez}>
+            lo<text>pez </text>
+          </h1>
+          <h1 className={styles.cantero}>
+            can<text>tero </text>
+          </h1>
+        </div>
+        <h3>.developer</h3>
+      </div>
+      <div id="limit" />
     </>
   );
 };
